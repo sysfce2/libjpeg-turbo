e07ad5142cf85d7a8251e640e3b94d3a8d03b1f3
DRC <information@libjpeg-turbo.org>
2026-07-27 16:20:57 -0400

    Build: Improve RIGHT_SHIFT_IS_UNSIGNED test
    Communicate test error to build system, so it can throw a fatal error.
    This eliminates the escaped text in the test code that triggered a
    CMP0219 policy warning with CMake 4.4.
    
    Fixes #905

---
+++ cmakescripts/right_shift_is_unsigned.c
@@ -0,0 +1,14 @@
+int main(void) {
+  long res = -0x7F7E80B1L >> 4;
+
+  if (res == -0x7F7E80CL)
+    return 0; /* right shift is signed */
+
+  /* see if unsigned-shift hack will fix it. */
+  /* we can't just test exact value since it depends on width of long... */
+  res |= 0xFFFFFFFFL << (32-4);
+  if (res == -0x7F7E80CL)
+    return 1; /* right shift is unsigned */
+
+  return -1; /* fatal error */
+}
